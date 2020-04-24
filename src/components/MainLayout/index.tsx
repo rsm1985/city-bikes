1fc76320472dc0c1d4d3a8596bc3106f190c0a60
rudenko-serg@ya.ru <Index111!>
2020-04-24 11:29:21 +0300

    Add styles

---
+++ src/components/MainLayout/index.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function MainLayout() {
+  return <div></div>;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./redux/reducers";
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 const composeEnhancers =
-    (typeof window !== "undefined" && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-    compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+  (typeof window !== "undefined" &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
-<Provider store={store}>
-    <App />
-</Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
