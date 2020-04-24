5682167e098930dd6e6fc15508b81bcc6cb0f06d
rudenko-serg@ya.ru <Index111!>
2020-04-24 15:18:17 +0300

    Add first active station by default

--- src/redux/networks/thunk.ts
+++ src/redux/networks/thunk.ts
@@ -1,18 +1,23 @@
 import axios from "axios";
 import { actionGetNetworks } from "redux/networks/actions";
+import { thunkGetStations } from "redux/stations/thunk";
 
-export const thunkGetNetworks = () => async (dispatch: any) => {
+export const thunkGetNetworks = () => async (dispatch: any, getState: any) => {
   try {
     const result = await axios.get("http://api.citybik.es/v2/networks");
     if (result) {
-      const data = result.data.networks.map(
-        ({ id, name, company, href, location }: any) => ({
+      const data = result.data.networks
+        .splice(1)
+        .map(({ id, name, company, href, location }: any) => ({
           id,
           href,
           title: `${company}, ${location.city}, ${location.country}`,
-        })
-      );
+        }));
       dispatch(actionGetNetworks(data));
+      const state = getState();
+      if (!state.citybikesStations.data) {
+        dispatch(thunkGetStations(data[0].href));
+      }
     }
   } catch (err) {
     console.log("Error: ", err);
--- src/redux/stations/thunk.ts
+++ src/redux/stations/thunk.ts
@@ -5,7 +5,6 @@ export const thunkGetStations = (href: string) => async (dispatch: any) => {
   try {
     const result = await axios.get(`http://api.citybik.es${href}`);
     if (result) {
-      console.log("result.data.network", result.data.network);
       const data = result.data.network.stations.map(
         ({ id, href, name }: any) => ({
           id,
